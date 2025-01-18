b7f839c5607f56b7cd72c0bd2dc960d21606ad5d
MenWic <lamr467@gmail.com>
2025-01-17 18:29:51 -0600

    listo

--- Log-in/app/models/config.php
+++ Log-in/app/models/config.php
@@ -1,3 +1,4 @@
 <?php 
  $con = mysqli_connect("localhost","root","","control") or die("Conexion fallida");
+ //Users.php (Entidad de DB con getters and setters), controller_user = enviar una clase users.php (que contenga las variables para insertar a DB)
 ?>
